3ddbba71d00c8ca445ce3e1a018c71a8a7f82b1b
rastgeleo <rastgeleo@gmail.com>
2020-06-19 15:47:07 +0100

    changed todo test

--- todo/tests.py
+++ todo/tests.py
@@ -1,69 +1,106 @@
-from datetime import timedelta
-
 from django.test import TestCase
-from django.utils import timezone
 
 from .models import Task, TaskList
 
 
 class TaskMainTest(TestCase):
 
-    def setUp(self):
-        self.tasklist = TaskList.objects.create(name='my list')
-
-    def test_uses_main_template(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'todo/task_main.html')
-
-    def test_displays_uncompleted_items_only(self):
+    def test_displays_task(self):
+        new_list = TaskList.objects.create(name='new list', slug='new-list')
         Task.objects.create(
-            name='item1', slug='item1', text='item1', completed=False,
-            tasklist=self.tasklist)
-        Task.objects.create(
-           name='item2', slug='item2', text='item2', completed=True,
-           tasklist=self.tasklist)
+            name='new task',
+            slug='new-task',
+            tasklist=new_list)
 
         response = self.client.get('/')
-        self.assertIn('item1', response.content.decode())
-        self.assertNotIn('item2', response.content.decode())
+        self.assertContains(response, 'new task')
 
     def test_can_save_a_POST_request(self):
-        some_time = timezone.now() + timedelta(days=30)
-        time_str = some_time.strftime('%Y-%m-%d %H:%M:%S')
+        new_list = TaskList.objects.create(name='new list', slug='new-list')
         data = {
             'name': 'item',
             'text': 'item text',
-            'due': time_str,
-            'tasklist': self.tasklist
+            'tasklist': new_list.pk
             }
         self.client.post('/', data=data)
-        self.assertEqual(Task.objects.count(), 1)
+
         new_item = Task.objects.first()
+        self.assertEqual(Task.objects.count(), 1)
         self.assertEqual(new_item.text, 'item text')
 
-        # Django stores datetime as UTC in database
-        self.assertEqual(
-            timezone.localtime(new_item.due).strftime('%Y-%m-%d %H:%M:%S'),
-            time_str
-            )
-
     def test_redirects_after_POST(self):
+        new_list = TaskList.objects.create(name='new list', slug='new-list')
         data = {
             'name': 'item',
             'text': 'item text',
-            'tasklist': self.tasklist
+            'tasklist': new_list.pk
             }
-        response = self.client.post('/', data)
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        response = self.client.post('/', data=data)
+
+        self.assertRedirects(response, '/')
 
     def test_only_saves_items_when_POST(self):
         self.client.get('/')
         self.assertEqual(Task.objects.count(), 0)
 
 
-class TaskListDeleteTest(TestCase):
+class NewListTest(TestCase):
 
-    def test_uses_delete_template(self):
-        response = self.client.get('todo:tasklist_delete')
-        self.assertTemplateUsed(response, 'todo/tasklist_confirm_delete.html')
+    def test_can_save_a_POST_request(self):
+        self.client.post('/list/create/', data={'name': 'new list'})
+
+        self.assertEqual(TaskList.objects.count(), 1)
+        new_list = TaskList.objects.first()
+        self.assertEqual(new_list.name, 'new list')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/list/create/', data={'name': 'new list'})
+        new_list = TaskList.objects.first()
+        self.assertRedirects(response, f'/list/{new_list.slug}/')
+
+
+class TaskListTest(TestCase):
+    def test_saving_and_retrieving_tasklist(self):
+        new_list = TaskList()
+        new_list.name = 'new list'
+        new_list.slug = 'new-list'
+        new_list.save()
+
+        saved_list = TaskList.objects.first()
+        self.assertEqual(saved_list, new_list)
+
+    def test_saving_and_retrieving_task(self):
+        new_list = TaskList.objects.create(name='new list', slug='new-list')
+        new_task = Task()
+        new_task.name = 'new task'
+        new_task.slug = 'new-task'
+        new_task.test = 'new task description'
+        new_task.tasklist = new_list
+        new_task.save()
+
+        saved_task = Task.objects.first()
+        self.assertEqual(saved_task, new_task)
+
+    def test_saving_and_retrieving_multiple_task(self):
+        new_list = TaskList.objects.create(name='new list', slug='new-list')
+        task_one = Task()
+        task_one.name = 'new task'
+        task_one.slug = 'new-task'
+        task_one.test = 'new task description'
+        task_one.tasklist = new_list
+        task_one.save()
+
+        task_two = Task()
+        task_two.name = 'new task 2'
+        task_two.slug = 'new-task-two'
+        task_two.test = 'new task description'
+        task_two.tasklist = new_list
+        task_two.save()
+
+        task_count = Task.objects.count()
+        first_saved_task = Task.objects.all()[1]
+        second_saved_task = Task.objects.all()[0]
+
+        self.assertEqual(task_count, 2)
+        self.assertEqual(first_saved_task.name, 'new task')
+        self.assertEqual(second_saved_task.name, 'new task 2')
