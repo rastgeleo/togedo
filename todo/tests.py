10d811dda1d5d36346bcd65e8fa597f2f6f88985
rastgeleo <rastgeleo@gmail.com>
2020-06-23 15:42:55 +0100

    Add todo test for CRUD

--- todo/tests.py
+++ todo/tests.py
@@ -1,15 +1,10 @@
 from django.test import TestCase
+from django.urls import reverse
 
 from .models import Task, TaskList
 
 
-class NewTaskListTest(TestCase):
-
-    # def test_uses_taskdetail_template(self):
-    #     new_list = TaskList.objects.create(
-    #         name='new list', slug='new-list')
-    #     response = self.client.get(f'/list/{new_list.slug}/')
-    #     self.assertTemplateUsed(response, 'todo/tasklist_detail.html')
+class TaskListCreateTest(TestCase):
 
     def test_uses_task_create_template(self):
         response = self.client.get('/list/create/')
@@ -26,6 +21,52 @@ class NewTaskListTest(TestCase):
         response = self.client.post('/list/create/', data={'name': 'new list'})
         new_list = TaskList.objects.first()
         self.assertRedirects(response, f'/list/{new_list.slug}/')
+
+
+class TaskListUpdateTest(TestCase):
+    def setUp(self):
+        self.tasklist = TaskList.objects.create(
+            name='new list', slug='new-list'
+        )
+
+    def test_can_save_a_POST_request(self):
+        data = {
+            'name': 'updated list',
+            'slug': 'updated-list'
+        }
+        self.client.post(
+            reverse(
+                'todo:tasklist_update',
+                kwargs={'slug': self.tasklist.slug}), data)
+        self.assertEqual(TaskList.objects.count(), 1)
+        self.assertEqual(TaskList.objects.first().name, data['name'])
+
+    def test_redirects_after_POST_request(self):
+        data = {
+            'name': 'updated list',
+            'slug': 'updated-list'
+        }
+        response = self.client.post(
+            reverse(
+                'todo:tasklist_update',
+                kwargs={'slug': self.tasklist.slug}), data)
+
+        self.assertRedirects(
+            response, reverse(
+                'todo:tasklist_detail', kwargs={'slug': data['slug']}))
+
+
+class TestListDeleteTest(TestCase):
+
+    def test_can_delete_a_POST_request(self):
+        new_list = TaskList.objects.create(name='new list', slug='new-list')
+        self.client.post(f'/list/{new_list.slug}/delete/')
+        self.assertEqual(TaskList.objects.count(), 0)
+
+    def test_redirects_after_POST(self):
+        new_list = TaskList.objects.create(name='new list', slug='new-list')
+        response = self.client.post(f'/list/{new_list.slug}/delete/')
+        self.assertRedirects(response, reverse('todo:tasklist_list'))
 
 
 class TaskListViewTest(TestCase):
@@ -54,6 +95,99 @@ class TaskListViewTest(TestCase):
         response = self.client.get('/list/')
 
         self.assertNotContains(response, list_.name)
+
+
+class TaskListDetailTest(TestCase):
+
+    def test_uses_tasklist_detail_template(self):
+        tasklist = TaskList.objects.create(name="new list", slug="new-list")
+        response = self.client.get(f'/list/{tasklist.slug}/')
+        self.assertTemplateUsed(response, 'todo/tasklist_detail.html')
+
+    def test_display_task_in_tasklist(self):
+        tasklist = TaskList.objects.create(name="new list", slug="new-list")
+        task = Task.objects.create(
+            name="new task", slug="new-task", tasklist=tasklist)
+        response = self.client.get(f'/list/{tasklist.slug}/')
+        self.assertContains(response, task.name)
+
+
+class TaskCreateTest(TestCase):
+
+    def setUp(self):
+        self.tasklist = TaskList.objects.create(
+            name="new list", slug="new-list")
+
+    def test_uses_task_form_template(self):
+        response = self.client.get(f'/list/{self.tasklist.slug}/create/')
+        self.assertTemplateUsed(response, 'todo/task_form.html')
+
+    def test_can_save_post_request(self):
+        data = {
+            'name': 'new task',
+            'slug': 'new-task',
+            'tasklist': self.tasklist.pk
+        }
+        self.client.post(f'/list/{self.tasklist.slug}/create/', data)
+        self.assertEqual(Task.objects.count(), 1)
+        self.assertEqual(Task.objects.first().name, data['name'])
+
+    def test_redirects_after_post(self):
+        data = {
+            'name': 'new task',
+            'slug': 'new-task',
+            'tasklist': self.tasklist.pk
+        }
+        response = self.client.post(
+            f'/list/{self.tasklist.slug}/create/', data)
+        self.assertRedirects(response, f'/list/{self.tasklist.slug}/')
+
+
+class TaskUpdateTest(TestCase):
+    def setUp(self):
+        self.tasklist = TaskList.objects.create(
+            name="new list", slug="new-list")
+        self.task = Task.objects.create(
+            name="new task", slug="new-task", tasklist=self.tasklist)
+
+    def test_can_save_post_request(self):
+        data = {
+            'name': 'updated task',
+            'slug': 'updated-task',
+            'tasklist': self.tasklist.pk
+        }
+        self.client.post(
+            f'/list/{self.tasklist.slug}/{self.task.slug}/update/', data)
+        self.assertEqual(Task.objects.count(), 1)
+        self.assertEqual(Task.objects.first().name, data['name'])
+
+    def test_redirects_after_post(self):
+        data = {
+            'name': 'new task',
+            'slug': 'new-task',
+            'tasklist': self.tasklist.pk
+        }
+        response = self.client.post(
+            f'/list/{self.tasklist.slug}/{self.task.slug}/update/', data)
+        self.assertRedirects(response, f'/list/{self.tasklist.slug}/')
+
+
+class TaskDeleteTest(TestCase):
+    def setUp(self):
+        self.tasklist = TaskList.objects.create(
+            name="new list", slug="new-list")
+        self.task = Task.objects.create(
+            name="new task", slug="new-task", tasklist=self.tasklist)
+
+    def test_can_save_post_request(self):
+        self.client.post(
+            f'/list/{self.tasklist.slug}/{self.task.slug}/delete/')
+        self.assertEqual(Task.objects.count(), 0)
+
+    def test_redirects_after_post(self):
+        response = self.client.post(
+            f'/list/{self.tasklist.slug}/{self.task.slug}/delete/')
+        self.assertRedirects(response, f'/list/{self.tasklist.slug}/')
 
 
 class TaskListModelTest(TestCase):
