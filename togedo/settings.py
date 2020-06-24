69716f4d880e2d8200702076622bd712012a515d
rastgeleo <rastgeleo@gmail.com>
2020-06-24 15:19:19 +0100

    Add gunicorn to requirements.txt

--- togedo/settings.py
+++ togedo/settings.py
@@ -125,7 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+if 'DJANGO_DEBUG_FALSE' in os.environ:
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+else:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, "static"),
+    ]
